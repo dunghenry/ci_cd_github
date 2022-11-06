4a1c7f33279469d08ad0e074ec9ffff9e5e83859
dunghenry <trandungksnb6930082001>
2022-11-06 23:34:49 +0700

    change file test

--- function.test.js
+++ function.test.js
@@ -20,11 +20,11 @@ describe('Add two numbers', () => {
     });
 });
 
-// describe('Minus two numbers', () => {
-//     test('Numeric inputs should give the answer', () => {
-//         expect(minus(8, 7)).toEqual(8 - 7);
-//     });
-//     test('Mixing numeric inputs should give the answer', () => {
-//         expect(minus(9, 7)).toEqual(9 - 7);
-//     });
-// });
+describe('Minus two numbers', () => {
+    test('Numeric inputs should give the answer', () => {
+        expect(minus(8, 7)).toEqual(8 - 7);
+    });
+    test('Mixing numeric inputs should give the answer', () => {
+        expect(minus(9, 7)).toEqual(9 - 7);
+    });
+});
