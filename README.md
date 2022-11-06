15c6698cc3f0bd1d13f096fd5eb53aa7e1bd3b22
dunghenry <trandungksnb6930082001>
2022-11-06 23:21:11 +0700

    add branch coverage

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 ## CI CD Github Actions
+
 ## Hihi
+
+## Run test
+
+```js
+npm test
+```
+
+## Run test coverage
+
+```js
+npm test --  --coverage
+```
--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+const { add, minus } = require('./function');
+const util = require('util');
 function sum(a, b) {
     return a + b;
 }
-const rs = sum(1, 2);
-console.log(rs);
-console.log(1 + 3);
+// const rs = sum(1, 2);
+// console.log(rs);
+// console.log(1 + 3);
+
+const a = 2;
+const b = 9;
+console.log(util.format('Add: %d + %d = %d', a, b, add(a, b)));
+console.log(util.format('Minus: %d - %d= %d', a, b, minus(a, b)));
